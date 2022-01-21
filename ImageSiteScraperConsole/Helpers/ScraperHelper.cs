67fd87288ba3d46d3d5ff1ec778a83687a11ac0c
jacob-oreilly <jakeor45@gmail.com>
2022-01-20 22:32:33 -0800

    added failing tests as a template for ScraperHelpers.

--- ImageSiteScraperConsole/Helpers/ScraperHelper.cs
+++ ImageSiteScraperConsole/Helpers/ScraperHelper.cs
@@ -6,5 +6,24 @@ namespace ImageSiteScraperConsole.Helpers
 {
     public class ScraperHelper
     {
+        public static object CallUrl()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static object GetHrefTags(string v1, string v2, string v3)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static object GetImageTags(string v1, string v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static object CrawlSite(HashSet<string> hashSet, string v1, int v2, int v3, string v4)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- ImageSiteScraperConsoleTest/HelpersTests/ScraperHelpersTest.cs
+++ ImageSiteScraperConsoleTest/HelpersTests/ScraperHelpersTest.cs
@@ -2,10 +2,38 @@
 using System.Collections.Generic;
 using System.Text;
 using ImageSiteScraperConsole.Helpers;
+using Xunit;
 
 namespace ImageSiteScraperConsoleTest.HelpersTests
 {
     public class ScraperHelpersTest
     {
+        [Fact]
+        public void CallUrlTest() {
+            var expected = "";
+            var actual = ScraperHelper.CallUrl();
+            Assert.Equal(actual, expected);
+        }
+
+        [Fact]
+        public void GetHrefTagsTest() {
+            var expected = "";
+            var actual = ScraperHelper.GetHrefTags("", "", "");
+            Assert.Equal(actual, expected);
+        }
+
+        [Fact]
+        public void GetImageTagsTest() {
+            var expected = "";
+            var actual = ScraperHelper.GetImageTags("", "");
+            Assert.Equal(expected, actual);
+        }
+
+        //[Fact]
+        //public void CrawlSiteTest() {
+        //    var expected = "";
+        //    var actual = ScraperHelper.CrawlSite(new HashSet<string>(), "", 0, 0, "");
+        //    Assert.Equal(expected, actual);
+        //}
     }
 }
